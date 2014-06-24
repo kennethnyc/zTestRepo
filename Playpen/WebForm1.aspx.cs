12cfdbdfc386bcb1ec3eb71827f19157f71c63a9
Kenneth <kennethnyc@gmail.com>
2014-06-24 12:14:37 +0800

    i have make changes to webform1

--- Playpen/WebForm1.aspx.cs
+++ Playpen/WebForm1.aspx.cs
@@ -10,8 +10,8 @@ namespace Playpen
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
+        { 
+               string xx = "test";
         }
     }
 }
